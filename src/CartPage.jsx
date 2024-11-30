faae9b5ced7ce11e0a377e905c1ef900edff3250
Gregory Murray <gr.j.murray@gmail.com>
2024-11-30 12:16:54 -0800

    CartPage, localstorage

--- src/CartPage.jsx
+++ src/CartPage.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 
 const CartPage = () => {
-    return (
-        <div>
-            <header><NavBar /></header>
-        </div>
-    );
-}
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const storedCartItems = JSON.parse(localStorage.getItem('cartItems')) || []
+    setCartItems(storedCartItems)
+  }, [])
+
+
+  return (
+    <div>
+      <header><NavBar /></header>
+      <h1>Shopping Cart</h1>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <ul>
+          {cartItems.map(item => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <img src={item.image} alt={item.name} />
+              <p>{item.description}</p>
+              <p>Seller: {item.seller}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default CartPage;
