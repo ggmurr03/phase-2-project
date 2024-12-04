814f4e5ff7db02d1b93816bea3ab31d85e5e4e22
Stuart Forrester <stforrester@gmail.com>
2024-12-04 14:36:38 -0600

    adds initial styling and updated medieval font to navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,14 +1,20 @@
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
+const StyledNav = styled.nav`
+  background: papayawhip;
+  font-family: MedievalSharp;
+  font-size:
+`
 
 const NavBar = () => {
     return (
-        <nav>
+        <StyledNav>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/sellers">Sellers</NavLink>
       <NavLink to="/shop">Shop</NavLink>
       <NavLink to="/cart">Cart</NavLink>
-        </nav>
+        </StyledNav>
     );
 }
 
